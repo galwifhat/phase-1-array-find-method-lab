217b404d17bf5b4683c01260a1831c0f67fe959a
Ruth <cirurue@gmail.com>
2025-04-01 10:01:17 +0300

    Array Find Method

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // code your solution here
+
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+];
+
+function superbowlWin(records) {
+  const winningRecord = records.find(function (record) {
+    if (record.result === "W") {
+      return true; // Found a win!
+    } else {
+      return false; // Keep searching
+    }
+  });
+
+  if (winningRecord) {
+    return winningRecord.year;
+  } else {
+    return undefined;
+  }
+}
+
+console.log(superbowlWin(record));
+/* 
+//arrow function
+const superbowlWin = (records) => {
+  const win = records.find((record) => record.result === "W");
+  return win ? win.year : undefined;
+}; */
